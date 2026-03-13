4be5908dc11624d518d3aa308c7cbd11aa3d0eeb
anyajiang0 <anya@whsrobotics.org>
2026-03-13 12:30:53 -0400

    testing

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 """
-THIS IS A SIMULATION, THERE IS *NO* REAL MONEY INVOLVED. I DO NOT ENCOURAGE GAMBLING, AND THIS IS NOT A GAMBLING GAME. IT IS A MARKET SIMULATOR!!!
+THIS IS A SIMULATION, THERE IS *NO* REAL MONEY INVOLVED.
+THIS SIMULATION IS NOT INTENDED TO ENCOURAGE GAMBLING IN ANY WAY, SHAPE, OR FORM.
 """
+# imports 
+
+import http.server
+import json
+import urllib.parse 
+
+# settings
+
+
+# spread calc (bear with me here)
+home_court_advantage = 3.0
+# home_rating and away_rating are calculated using data from an API 
+spread = home_rating - away_rating + home_court_advantage 
